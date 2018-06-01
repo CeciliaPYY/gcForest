264ab4a5d25e5eea7c83eaad03e7fdf051d92659
Cecilia <30582437+CeciliaPYY@users.noreply.github.com>
2018-06-01 10:39:46 +0800

    add lib path to sys.path

--- examples/demo_mnist.py
+++ examples/demo_mnist.py
@@ -14,11 +14,21 @@ from keras.datasets import mnist
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-sys.path.insert(0, "lib")
+#sys.path.insert(0, "lib")
 
 from gcforest.gcforest import GCForest
 from gcforest.utils.config_utils import load_json
 
+
+def add_path(path):
+    if path not in sys.path:
+        sys.path.insert(0, path)
+        
+this_dir = osp.dirname(__file__)
+
+# Add lib to PYTHONPATH
+lib_path = osp.join(this_dir, '..', 'lib')
+add_path(lib_path)
 
 def parse_args():
     parser = argparse.ArgumentParser()
